d925de42fc6fe89b333c140d9a1b6d12a062727b
ronakjpatel <40088060+ronakjpatel@users.noreply.github.com>
2024-03-30 15:46:38 -0400

    add more algos

---
+++ Arrays/Top_K_Elements.py
@@ -0,0 +1,55 @@
+'''
+#347 
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+'''
+
+def topKFrequent(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: List[int]
+    """
+    
+    my_dict = {}
+    #iterate through each number
+    final_arr=[]
+    for num in nums:
+        my_dict[num] = my_dict.get(num,0) + 1
+    
+    aux_array = [[] for x in range(len(nums) + 1 )] #will add one more because we wont use 0 index or 0 freq as a value
+
+    for each_num,freq in my_dict.items():
+        aux_array[freq].append(each_num)
+        
+    for i in range(len(aux_array)-1,0,-1):
+        for kk in aux_array[i]:
+            final_arr.append(kk)
+            if len(final_arr) ==k:
+                return final_arr
+        
+    
+
+    
+
+num_arr = [1,2]
+k = 2
+
+print(topKFrequent(num_arr,k))
+#efficient solution 
+
+
+    
+
+
+
